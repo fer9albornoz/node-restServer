a725c9ef0a5fd1a5a4d2f55a560fd45733a9146f
Fernando Albornoz <fer9albornoz@gmail.com>
2021-04-06 01:40:54 -0300

    first version node server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/usuario', (req, res) => {
+    res.json('get usuario');
+});
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok: false,
+            message: 'El nombre es necesario'
+        })
+    }else{
+        res.json({
+            body
+        });
+    }
+});
+app.put('/usuario/:id', (req, res) => {
+    res.json('put usuario');
+});
+app.delete('/usuraio', (req, res) => {
+    res.json('get delete');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Listen on port : ', process.env.PORT);
+});
