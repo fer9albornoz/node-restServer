dee40cdd5030f0ccb3357c77675eea7207e6ccee
Fernando Albornoz <fer9albornoz@gmail.com>
2021-04-13 18:47:12 -0300

    rest server - usuario - variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://fernandoAlbor:iBErCO9OuP3l77eL@cluster0.unkw2.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
