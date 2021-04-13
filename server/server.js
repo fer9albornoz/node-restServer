eddaa7c1e290cc85472f8fd76f9a4066abfb610f
Fernando Albornoz <fer9albornoz@gmail.com>
2021-04-13 18:10:40 -0300

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('./config/config');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
@@ -7,32 +8,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario');
-});
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            message: 'El nombre es necesario'
-        })
-    }else{
-        res.json({
-            body
-        });
-    }
-});
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-app.delete('/usuraio', (req, res) => {
-    res.json('get delete');
-});
+app.use( require('./routes/usuario') );
 
+mongoose.connect(process.env.URLDB ,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (err, res) => {
+    if(err) throw err;
+    console.log('MongoDB connected');
+});
 app.listen(process.env.PORT, () => {
     console.log('Listen on port : ', process.env.PORT);
 });
